07217ebf45e66c54311b98023d67f638dd24e4af
Luckylooker <newlooker@126.com>
2024-11-15 20:30:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-mkdir openwrt-packages
-cd openwrt-packages
+# mkdir openwrt-packages
+# cd openwrt-packages
 git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
 git clone https://github.com/kenzok8/small.git  package/openwrt-small
